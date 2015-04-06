2e8d239bb0169a6249acc68f11fdbbc54417027d
mkossarian <mkossarian@gmail.com>
2015-04-06 01:52:01 -0700

    add submit button

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-    ('#due').datepicker();
-});
+var main function() {
+    $('.submit').click(function() {
+        $('body').animate({
+            left:'900px'}, 200);
+    })
+};
+
+$(document).ready(main);
